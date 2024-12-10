048da71beaac639acf1d2421a12add4df3a6c0de
Zach Donnelly <zachary.donnelly@walmart.com>
2024-12-10 12:55:24 -0500

    feat: finish basic sign in mock

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,14 +1,17 @@
-import { useSignIn } from '@clerk/clerk-expo'
-import { Link, useRouter } from 'expo-router'
-import React from 'react'
-import { Button, Text, TextInput, View } from 'react-native'
+import CustomTextInput from "@/components/custom/TextInput"
+import RadialGradientBackground from "@/components/gradient/RadialGradientBackground"
+import SignUpModal from "@/components/modal/SignUpModal"
+import { useSignIn } from "@clerk/clerk-expo"
+import { useRouter } from "expo-router"
+import React from "react"
+import { ScrollView, Text, TouchableOpacity, View } from "react-native"
 
 const SignInScreen = () => {
   const { signIn, setActive, isLoaded } = useSignIn()
   const router = useRouter()
 
-  const [emailAddress, setEmailAddress] = React.useState('')
-  const [password, setPassword] = React.useState('')
+  const [emailAddress, setEmailAddress] = React.useState("")
+  const [password, setPassword] = React.useState("")
 
   const onSignInPress = React.useCallback(async () => {
     if (!isLoaded) {
@@ -21,9 +24,9 @@ const SignInScreen = () => {
         password,
       })
 
-      if (signInAttempt.status === 'complete') {
+      if (signInAttempt.status === "complete") {
         await setActive({ session: signInAttempt.createdSessionId })
-        router.replace('/')
+        router.replace("/")
       } else {
         console.error(JSON.stringify(signInAttempt, null, 2))
       }
@@ -33,27 +36,71 @@ const SignInScreen = () => {
   }, [isLoaded, emailAddress, password])
 
   return (
-    <View>
-      <TextInput
-        autoCapitalize="none"
-        value={emailAddress}
-        placeholder="Email..."
-        onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
-      />
-      <TextInput
-        value={password}
-        placeholder="Password..."
-        secureTextEntry={true}
-        onChangeText={(password) => setPassword(password)}
-      />
-      <Button title="Sign In" onPress={onSignInPress} />
-      <View>
-        <Text>Don't have an account?</Text>
-        <Link href="/sign-up">
-          <Text>Sign up</Text>
-        </Link>
+    <RadialGradientBackground style={{ flex: 1 }}>
+      <View className='flex-1'>
+        <SignUpModal visible>
+          <ScrollView
+            contentContainerStyle={{
+              flex: 1,
+              alignItems: "center",
+              backgroundColor: "rgba(255, 255, 255, 0.9)",
+            }}>
+            <View className='flex-1 max-h-[125px] items-start py-0 px-5'>
+              <Text className='text-3xl font-bold text-left max-w-[300px] text-[#101828]'>
+                Welcome back!
+              </Text>
+              <Text className='max-w-[280px] text-left mt-3 text-[#101828] text-lg'>
+                Please enter your email address and password to create an
+                account.
+              </Text>
+            </View>
+            <View className='gap-y-6'>
+              <CustomTextInput
+                autoCapitalize='none'
+                value={emailAddress}
+                placeholder='john@yourdomain.com'
+                labelText='Email'
+                onChangeText={(email) => setEmailAddress(email)}
+              />
+              <CustomTextInput
+                value={password}
+                placeholder='Password...'
+                secureTextEntry={true}
+                labelText='Enter password'
+                onChangeText={(password) => setPassword(password)}
+              />
+            </View>
+            <View className='flex-0 items-center'>
+              <TouchableOpacity
+                disabled={!emailAddress || !password}
+                style={{
+                  backgroundColor:
+                    !emailAddress || !password ? "#D9D6FE" : "#6938EF",
+                }}
+                className='w-[303px] h-14 justify-center items-center self-center rounded-2xl mt-20'
+                onPress={onSignInPress}>
+                <Text className='text-white text-lg font-bold'>Sign In</Text>
+              </TouchableOpacity>
+              <TouchableOpacity className='mt-2'>
+                <Text className='font-bold text-lg color-[#6938EF]'>
+                  Forgot password?
+                </Text>
+              </TouchableOpacity>
+              <Text className='mt-32 text-md text-[#667085]'>
+                Don't have an account?
+              </Text>
+              <TouchableOpacity
+                className='items-center self-center bg-transparent'
+                onPress={() => router.replace("/(auth)/sign-up")}>
+                <Text className='mt-1 text-base font-semibold text-[#6938EF]'>
+                  Sign Up
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </SignUpModal>
       </View>
-    </View>
+    </RadialGradientBackground>
   )
 }
 
